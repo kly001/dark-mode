50c8ec1dfa7003484537fab95439bb59e231ba6c
kly001 <karenly1961@gmail.com>
2019-09-13 01:24:19 -0400

    Added More Notes

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -4,14 +4,26 @@ import useLocalStorage from "./useLocalStorage";
 export const useDarkMode = ()=> {
   
     const [storedValue, setstoredValue] = useLocalStorage("darkMode")
-    
+
+    // check to see if the value from `useLocalStorage` is true or false.
     useEffect(() => {
+    //If it's true, add the class `dark-mode` to the `body` element.
         if (storedValue) {
-           window.document.body.classList.add("dark-mode")
+           document.body.classList.add("dark-mode")
+    //If it's false, remove the class from the `body` element. 
         } else {
-            window.document.body.classList.remove("dark-mode")
+            document.body.classList.remove("dark-mode")
         }
 
     }, [storedValue]);
+    //return something out of here so we can use this in our app
     return [storedValue, setstoredValue]
 }
+
+// Resources:
+
+// 1. https://davidwalsh.name/classlist
+// 2. gregnb/react-to-print(GitHub)
+// 3. https://www.w3schools.com/jsref/prop_element_classlist.asp
+// 4. https://alligator.io/js/classlist/
+
