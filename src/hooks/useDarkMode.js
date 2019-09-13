9e3af9f1d444e75eb2f3321ed358cfa3f011ca5f
kly001 <karenly1961@gmail.com>
2019-09-13 00:32:53 -0400

    DarkMode Working (HOORAY!!!)

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import useLocalStorage from "./useLocalStorage";
 
-const useDarkMode = (key, initialValue)=> {
-    console.log(key)
-    const [storedMode, setStoredMode] = useLocalStorage("darkMode")
-    return (
-     
-        <p>This is Darkmode component</p>
-    )
+export const useDarkMode = ()=> {
+  
+    const [storedValue, setstoredValue] = useLocalStorage("darkMode")
+    
+    useEffect(() => {
+        if (storedValue) {
+           window.document.body.classList.add("dark-mode")
+        } else {
+            window.document.body.classList.remove("dark-mode")
+        }
+
+    }, [storedValue]);
+    return [storedValue, setstoredValue]
 }
-export default useDarkMode
